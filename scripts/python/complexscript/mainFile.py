e157cba5c5571cae34a2bb95007ce0ca5d52d413
Melonia Mendonca <meloniam@amazon.com>
2017-10-24 11:02:00 -0700

    Update mainFile.py

--- scripts/python/complexscript/mainFile.py
+++ scripts/python/complexscript/mainFile.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import sys
-from unixPython import RunHelloWorld
+from childFile import RunHelloWorld
 
-myworld = RunHelloWorld("See you later")
+myworld = RunHelloWorld(sys.argv[0])
 myworld.printComment()
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
