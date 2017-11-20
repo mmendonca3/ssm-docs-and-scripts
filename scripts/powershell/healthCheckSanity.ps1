3d34b931d8fff5959726b45800d04fb8432304ee
Melonia Mendonca <meloniam@amazon.com>
2017-11-20 12:57:20 -0800

    Update healthSanityScript with versions of SSM Agent and pV driver

--- scripts/powershell/healthCheckSanity.ps1
+++ scripts/powershell/healthCheckSanity.ps1
@@ -1,2 +1,11 @@
 
-Write-Host "Hello World"
+#Obtain the version of SSM Agent installed on the instance
+$agentInfo=Get-WmiObject -Class Win32_Product | Where-Object {$_ | Select-String "Amazon SSM Agent"}
+$agentInfo.Version
+
+#Check the patch baselines
+
+
+#Check the version of the PVDriver installed on the instance
+$pvDriver=Get-WmiObject -Class Win32_Product | Where-Object {$_ | Select-String "AWS PV Driver"}
+$pvDriver.Version
