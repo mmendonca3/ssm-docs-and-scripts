6e61044164a33d0b937470ee5fd2fdee54080f32
Melonia Mendonca <meloniam@amazon.com>
2017-11-20 14:45:04 -0800

    Add windows update check

--- scripts/powershell/healthCheckSanity.ps1
+++ scripts/powershell/healthCheckSanity.ps1
@@ -1,11 +1,10 @@
-
 #Obtain the version of SSM Agent installed on the instance
 $agentInfo=Get-WmiObject -Class Win32_Product | Where-Object {$_ | Select-String "Amazon SSM Agent"}
-$agentInfo.Version
+Write-Host "SSM Agent version is - " $agentInfo.Version
 
 #Check the patch baselines
-
+Get-WmiObject -Class "win32_quickfixengineering"
 
 #Check the version of the PVDriver installed on the instance
 $pvDriver=Get-WmiObject -Class Win32_Product | Where-Object {$_ | Select-String "AWS PV Driver"}
-$pvDriver.Version
+Write-Host "PV driver version installed is - " $pvDriver.Version
